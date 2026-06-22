3b4e28a94d9bd777ad902bfc51db34db4af38b6a
francescomendosa-create <francescomendosa@gmail.com>
2026-06-22 02:32:30 +0200

    s

--- remote-host/sw.js
+++ remote-host/sw.js
@@ -1,5 +1,5 @@
 /** PWA Service Remote: consente installazione + aggiornamenti; HTML sempre dalla rete. */
-const CACHE = 'svc-remote-pwa-v30';
+const CACHE = 'svc-remote-pwa-v31';
 self.addEventListener('install', (e) => {
   e.waitUntil(self.skipWaiting());
 });

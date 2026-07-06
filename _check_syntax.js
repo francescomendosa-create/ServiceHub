59abe10e4593d6ca60e84db3d6142d7e81418b05
francescomendosa-create <francescomendosa@gmail.com>
2026-07-06 11:22:50 +0200

    n

--- _check_syntax.js
+++ _check_syntax.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const html = fs.readFileSync(__dirname + '/index.html', 'utf8');
 const start = html.indexOf('<script type="module">');
 const end = html.indexOf('</script>', start);
-const code = html.slice(start + '<script type="module">'.length, end);
+let code = html.slice(start + '<script type="module">'.length, end);
+code = code.replace(/^\s*import\s+.+$/gm, '');
 try {
   new Function(code);
-  console.log('module script syntax OK');
+  console.log('module script syntax OK (imports stripped)');
 } catch (e) {
   console.error('module script syntax FAIL:', e.message);
   process.exit(1);
